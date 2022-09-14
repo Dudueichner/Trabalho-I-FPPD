2f6db18252592afcea0b0fd90be4e82422ad3204
nathan-rr-mello <111316842+nathan-rr-mello@users.noreply.github.com>
2022-09-14 10:40:46 -0300

    criada solucao sequencial em go

---
+++ solucaoSequencial.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	phil = []string{"Aristoteles", "Platao", "Socrates", "Tales", "Nietzsche"}
+	repeticoes = 1
+)
+
+func eat(name string){
+	fmt.Println(name + " esta comendo");
+}
+
+func think(name string){
+	fmt.Println(name + " esta pensando");
+}
+
+func main(){
+
+	for k := 0; k < repeticoes; k++ {
+		for i := 0; i < len(phil); i++ {
+			eat(phil[i])
+			for j := 0; j < len(phil); j++ {
+				if j != i {
+					think(phil[j]);
+				}
+			}
+			fmt.Println("<----------------------------------->")
+			time.Sleep(time.Second)
+		}	
+	}
+	
+}
